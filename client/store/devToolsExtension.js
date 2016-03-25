c60672318abfb555b0ff74178569596ec0e8ffc7
Randy Coulman <rcoulman@gmail.com>
2016-03-25 14:01:13 -0700

    Fix the guard against a missing devToolsExtension
    The existing logic raises an error if the extension is not available in
    a development environment

--- client/store/devToolsExtension.js
+++ client/store/devToolsExtension.js
@@ -3,8 +3,8 @@ import { identity } from 'ramda'
 const isProduction =
   process.env.NODE_ENV === 'production' // eslint-disable-line no-process-env
 
-const devToolsExtension = isProduction
+const devToolsExtension = isProduction || !window.devToolsExtension
   ? identity
-  : (window.devToolsExtension() || identity)
+  : window.devToolsExtension()
 
 export default devToolsExtension
