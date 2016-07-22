97693ee7c678c531996a81fb37a73db4f4240993
Byron Matto <bmatto@gmail.com>
2016-07-22 16:11:55 -0400

    [WIP] - gitignore coverage - update karma cons to use mocha and coverage reporters - add istanbul, babel coverage plugin, karma coverage, and karma mocha reporter to package.json - configure babel coverage plugin in package.json

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ const webpackConfig = require('./webpack.config')
 // eslint-disable-next-line no-process-env
 process.env.BABEL_ENV = 'test' // so we load the correct babel plugins
 const testGlob = '**/*-spec.js'
-// const srcGlob = '**/*!(-spec).js'
+const srcGlob = '**/*!(-spec).js'
 
 module.exports = function setKarmaConfig(config) {
   config.set({
@@ -12,21 +12,27 @@ module.exports = function setKarmaConfig(config) {
     browsers: ['Chrome'],
     colors: true,
     concurrency: Infinity,
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', dir: '../coverage/', subdir: '.' },
+        { type: 'json', dir: '../coverage/', subdir: '.' },
+        { type: 'text-summary' }
+      ]
+    },
     files: [{
       pattern: testGlob,
       watched: false,
       served: true,
       included: true
-    }],
-    // files: [testGlob, srcGlob],
+    }, testGlob],
     frameworks: ['mocha', 'chai-as-promised', 'chai'],
     logLevel: config.LOG_INFO,
     port: 9876,
     preprocessors: {
-      [testGlob]: ['webpack'] // ,
-      // [srcGlob]: ['webpack']
+      [testGlob]: 'webpack',
+      [srcGlob]: ['webpack']
     },
-    reporters: ['progress'],
+    reporters: ['mocha', 'coverage'],
     singleRun: true,
     webpack: webpackConfig,
     webpackMiddleware: { noInfo: true }
