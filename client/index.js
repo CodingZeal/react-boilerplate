df105ddeca792f5456350b7a7c9b1b7c4fc40774
Byron Matto <bmatto@gmail.com>
2016-11-23 10:02:27 -0500

    Simplify polyfill transform and transform configuration

--- client/index.js
+++ client/index.js
@@ -1,4 +1,3 @@
-import es6promise from 'es6-promise'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -6,8 +5,6 @@ import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import { Router, configureStore } from 'base'
-
-es6promise.polyfill()
 
 const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
